d3431fa046979392d460d939b2e84725483105ea
ReinisGrinbergs <125680978+ReinisGrinbergs@users.noreply.github.com>
2023-04-17 01:12:27 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,40 @@
-# python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    data = input()
+    if data[0]=="I":
+        pattern=input()
+        text = input()
+    elif data[0]=="F":
+        inputf = '06'
+        inputf = "tests/" + inputf
+       
+        with open(inputf, 'r') as file:
+                pattern, text = file.read().splitlines()
+    return (pattern.rstrip(), text.rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
-
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
-
-    # and return an iterable variable
-    return [0]
-
+    lP = len(pattern)
+    lT = len(text)
+    hP = hash(pattern)
+    hT = hash(text[:lP])
+    output = []
+    for i in range(lT-lP+1):
+        if hP == hT:
+            if pattern == text[i:i+lP]:
+                output.append(i)
+            else:
+                for j in range(lP):
+                    if text[i+j] != pattern[j]:
+                        break
+                else:
+                    output.append(i)
+        if i < lT-lP:
+            hT = hash(text[i+1:i+lP+1])
+    return output
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
